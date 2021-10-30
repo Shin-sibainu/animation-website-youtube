acac3d820576e256b71498043836ddebb42a7b84
Shin-sibainu <shincode0712@gmail.com>
2021-10-30 10:19:03 +0900

    change

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ tl.fromTo(hero, 1, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
   .fromTo(
     slider,
     1.2,
-    { x: "-100%" },
-    { x: "0%", ease: Power2.easeInOut },
+    { y: "-100%" },
+    { y: "0%", ease: Power2.easeInOut },
     "-=1.2"
   )
   .from(logo, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5")
